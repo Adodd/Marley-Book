2f14988bdb82106a9ace1bee5a3658fb356732dd
Alphie Dodd <alphiedodd@me.com>
2014-05-04 20:44:00 +0100

    haveing fun useing marleybook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marley::Application.config.secret_key_base = '698120fe4537711d6077ee8eea1cbaaca23cffc49a4e730bafc8167170b224ed96cab6b7c5dc31d1cb26167f5d508aa5ad9eef3bfd383698d5b40fbc793158e8'
